f81e5be11094e37d262d82edf2b8d43c6b3d7f54
Orlando Watson <ogw@orlandogwatson.com>
2018-08-07 21:34:54 -0700

    show navigation when page is at bottom

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 (function() {
+    var nav = document.querySelector("body nav");
     var navList = document.querySelector(".nav__list");
     var toggler = document.querySelector(".nav__toggle");
 
@@ -6,9 +7,11 @@
     var state = toggler.getAttribute("data-state");
 
     if (state === "closed") {
+      nav.setAttribute("data-state", "open");
       toggler.setAttribute("data-state", "open");
       navList.setAttribute("data-state", "open");
     } else {
+      nav.setAttribute("data-state", "closed");
       toggler.setAttribute("data-state", "closed");
       navList.setAttribute("data-state", "closed");
     }
@@ -19,6 +22,7 @@
     var innerWidth = window.innerWidth;
 
     if (innerWidth > 992 && state === "open") {
+      nav.setAttribute("data-state", "closed");
       toggler.setAttribute("data-state", "closed");
       navList.setAttribute("data-state", "closed");
     }
